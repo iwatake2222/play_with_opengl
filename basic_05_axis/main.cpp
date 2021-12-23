e6b71a24def2540fc183ea74dfd05552d47420b4
iwatake2222 <take.iwiw2222@gmail.com>
2021-12-23 13:50:03 +0900

    [basic_05_axis] Create by cloning basic_02_shader_cube_control, and change naming rule

---
+++ basic_05_axis/main.cpp
@@ -0,0 +1,148 @@
+/*** Include ***/
+/* for general */
+#include <stdint.h>
+#include <stdio.h>
+#include <fstream> 
+#include <vector>
+#include <string>
+#include <chrono>
+
+/* for GLFW */
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp> 
+
+#include "shader.h"
+#include "camera_control.h"
+#include "data.h"
+
+/*** Macro ***/
+/* macro functions */
+#define RUN_CHECK(x)                                         \
+  if (!(x)) {                                                \
+    fprintf(stderr, "Error at %s:%d\n", __FILE__, __LINE__); \
+    exit(1);                                                 \
+  }
+
+/* Settings */
+
+/*** Global variables ***/
+
+
+/*** Function ***/
+int main(int argc, char *argv[])
+{
+    /*** Initialize ***/
+    GLFWwindow* window;
+
+    /* Initialize GLFW */
+    RUN_CHECK(glfwInit() == GL_TRUE);
+
+    /* Create a window (x4 anti-aliasing, OpenGL3.3 Core Profile)*/
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    RUN_CHECK(window = glfwCreateWindow(720, 480, "main", NULL, NULL));
+    glfwMakeContextCurrent(window);
+
+    /* Initialize GLEW */
+    glewExperimental = true;
+    RUN_CHECK(glewInit() == GLEW_OK);
+
+    /* Ensure not to miss input */
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    glfwSetInputMode(window, GLFW_STICKY_MOUSE_BUTTONS, GL_TRUE);
+
+    /* Dark blue background */
+    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+    
+    /* Enable depth test */
+    glEnable(GL_DEPTH_TEST);
+    /* Accept fragment if it closer to the camera than the former one */
+    glDepthFunc(GL_LESS);
+    /* Cull triangles which normal is not towards the camera */
+    glEnable(GL_CULL_FACE);
+
+    /* Load shader */
+    GLuint programId = LoadShaders("resource/SimpleVertexShader.vertexshader", "resource/SimpleFragmentShader.fragmentshader");
+    GLuint matrixId = glGetUniformLocation(programId, "MVP");
+
+    /* Create Vertex Array Object */
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    /* Create Vertex Buffer Object and copy data */
+    GLuint vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+    GLuint colorBuffer;
+    glGenBuffers(1, &colorBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+
+    /* Initialize camera matrix controls (Initial position : on +Z, toward -Z) */
+    CameraControl_initialize(window, glm::vec3(0, 0, 5), 3.14f, 0.0f);
+
+    /*** Start loop ***/
+    while(1) {
+        /* Clear the screen */
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glUseProgram(programId);
+        /* Camera matrix */
+        CameraControl_update(window);
+        glm::mat4 Projection = CameraControl_getProjectionMatrix();
+        glm::mat4 View = CameraControl_getViewMatrix();
+
+        /* Model matrix */
+        glm::mat4 Model = glm::mat4(1.0f);
+        static float rotX = 0.0f;
+        glm::mat4 myRotationAxis = glm::rotate(rotX++ / (2 * 3.14f), glm::vec3(1, 0, 0));
+        Model = myRotationAxis * Model;
+
+        /* Calculate ModelViewProjection matrix and send it to shader */
+        glm::mat4 MVP = Projection * View * Model;
+        glUniformMatrix4fv(matrixId, 1, GL_FALSE, &MVP[0][0]);
+
+        /* Set attribute buffer */
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+        /* Draw the triangle */
+        glDrawArrays(GL_TRIANGLES, 0, sizeof(g_vertex_buffer_data) / sizeof(float) / 3);
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+        glUseProgram(0);
+
+        /* Swap buffers */
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+        /* Check if the ESC key was pressed or the window was closed */
+        if (glfwWindowShouldClose(window) != 0 || glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+            break;
+        }
+    }
+
+    /*** Finalize ***/
+    /* Cleanup VBO and shader */
+    glDeleteBuffers(1, &vertexBuffer);
+    glDeleteBuffers(1, &colorBuffer);
+    glDeleteVertexArrays(1, &vao);
+    glDeleteProgram(programId);
+
+    /* Close OpenGL window and terminate GLFW */
+    glfwTerminate();
+
+    return 0;
+}
